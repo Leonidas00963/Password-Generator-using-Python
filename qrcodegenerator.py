d4be7005b7e128385a24eaf388bc73c58a5c28d2
Leonidas00963 <74958520+Leonidas00963@users.noreply.github.com>
2021-02-18 18:29:30 +0100

    you must from CMD the // pip install qrcode // and // pip install img // installing
    there are very important module for Qr code Generating.

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -1,11 +1,16 @@
-import qrcode
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data('Some data')
-qr.make(fit=True)
 
-img = qr.make_image(fill_color="black", back_color="white")
+import pyqrcode
+import png
+from pyqrcode import QRCode
+
+# String which represents the QR code
+s = "Enter your Link or Text Here"
+
+# Generate QR code
+url = pyqrcode.create(s)
+
+
+url.svg("myqr.svg", scale = 8)
+
+
+url.png('myqr.png', scale = 6)
