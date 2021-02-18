86496e336fa94938f53de6c6f5b9c70913761587
Leonidas00963 <74958520+Leonidas00963@users.noreply.github.com>
2021-02-18 18:06:19 +0100

    you must from CMD the // pip install qrcode // and // pip install img // installing
    there are very important module for Qr code Generating.

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -1,15 +1,11 @@
 import qrcode
-
-
-
-qr = qrcode.QrCode(
+qr = qrcode.QRCode(
     version=1,
-    box_size=15,
-    border=5
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
 )
+qr.add_data('Some data')
+qr.make(fit=True)
 
-data = 'https://www.webfeinschliff.de/wp-content/uploads/2017/06/karrieretutor-dozent-daniel-werner-1024x1024.png'
-qr.add_data(data)
-qr.make(ƒit=true)
-img = qr.make_image(ƒill='black', back_color='white')
-img.save('Karriretutor Logo.png')
+img = qr.make_image(fill_color="black", back_color="white")
